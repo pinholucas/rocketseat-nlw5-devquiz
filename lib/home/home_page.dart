8a71baeb36d73f8d9daf1ced4a80e8cde9a58352
Lucas Pinho <erphis@gmail.com>
2021-04-19 22:07:11 -0300

    feat: add TagsList

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:devquiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:devquiz/home/widgets/tags_list/tags_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,6 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: TagsListWidget(),
     );
   }
 }
