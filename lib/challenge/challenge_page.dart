1cbeaba543f320d9d71ee8c3b14386db33ef0afa
Lucas Pinho <erphis@gmail.com>
2021-04-25 01:45:07 -0300

    feat: add custom scroll in quiz questions

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,6 +1,7 @@
 import 'package:devquiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:devquiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:devquiz/helpers/custom_lock_scroll_physics.dart';
 import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
@@ -24,10 +25,23 @@ class _ChallengePageState extends State<ChallengePage> {
   final controller = ChallengeController();
   final pageController = PageController();
 
+  ScrollPhysics questionsScrollPhysics = AlwaysScrollableScrollPhysics();
+
+  _onPageChanged(int page) {
+    questionsScrollPhysics = widget.questions[page + 1].isAnswered
+        ? AlwaysScrollableScrollPhysics()
+        : CustomLockScrollPhysics(lockRight: true);
+
+    pageController.animateToPage(page,
+        duration: Duration(milliseconds: 100), curve: Curves.linear);
+
+    setState(() {});
+  }
+
   @override
   void initState() {
     pageController.addListener(() {
-      controller.currentQuestion = pageController.page!.toInt() + 1;
+      controller.currentQuestion = pageController.page!.toInt();
     });
 
     super.initState();
@@ -42,13 +56,14 @@ class _ChallengePageState extends State<ChallengePage> {
           valueListenable: controller.currentQuestionNotifier,
           builder: (context, value, _) => QuestionIndicatorWidget(
             context: context,
-            currentQuestion: value == 0 ? 1 : value,
+            currentQuestion: value + 1,
             totalQuestions: widget.totalQuestions,
           ),
         ),
       ),
       body: PageView(
-        physics: NeverScrollableScrollPhysics(),
+        onPageChanged: _onPageChanged,
+        physics: questionsScrollPhysics,
         controller: pageController,
         children: widget.questions.map((e) => QuizWidget(question: e)).toList(),
       ),
@@ -60,16 +75,16 @@ class _ChallengePageState extends State<ChallengePage> {
             children: [
               NextButton.white(
                 label: 'Pular',
-                onTap: () {},
-              ),
-              SizedBox(width: 7),
-              NextButton.green(
-                label: 'Confirmar',
                 onTap: () {
                   pageController.nextPage(
                       duration: Duration(milliseconds: 100),
                       curve: Curves.linear);
                 },
+              ),
+              SizedBox(width: 7),
+              NextButton.green(
+                label: 'Confirmar',
+                onTap: () {},
               ),
             ],
           ),
