f9835f7f3805604eedbda99f6164f65a98db8311
Lucas Pinho <erphis@gmail.com>
2021-04-25 02:05:02 -0300

    feat: make SplashScreen functional

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,9 +1,19 @@
 import 'package:devquiz/core/core.dart';
+import 'package:devquiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 1)).then(
+      (_) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      ),
+    );
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
